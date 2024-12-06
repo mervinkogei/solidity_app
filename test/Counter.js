0ccafb406da31dc5122f9bded48ea44d4ab639a3
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-12-06 20:03:41 +0300

    Added the RealEstate Solidity

--- test/Counter.js
+++ test/Counter.js
@@ -20,11 +20,50 @@ describe('Counter', () => {
     })
 
     describe('Counting', () =>{
-        it('increments the counter', async ()=>{
-            let transaction = await counter.increment()
+        let transaction
 
+       it('reads the count from the "count" public variables', async () =>{
+        expect(await counter.count()).to.equal(1)
+       })
+
+       it('reads the count from the "getCount" function', async () =>{
+        expect(await counter.getCount()).to.equal(1)
+       })
+
+        it('increments the counter', async ()=>{
+            transaction = await counter.increment()
             await transaction.wait()
+
             expect(await counter.count()).to.equal(2)
+
+            transaction = await counter.increment()
+            await transaction.wait()
+            
+            expect(await counter.count()).to.equal(3)
         })
+
+        it('Decrements the counter', async ()=>{
+            transaction = await counter.decrement()
+            await transaction.wait()
+
+            expect(await counter.count()).to.equal(0)
+
+            //Cannot decrement below 0
+            await expect(counter.decrement()).to.be.reverted
+        })
+
+        it('reads the name from the "name" public variables', async () =>{
+            expect(await counter.name()).to.equal('My Counter')
+           })
+    
+           it('reads the name from the "getName" function', async () =>{
+            expect(await counter.getName()).to.equal('My Counter')
+           })
+
+           it('Updates the name', async () =>{
+            transaction = await counter.setName('New Name')
+            await transaction.wait()
+            expect(await counter.name())
+           })
     })
 })
